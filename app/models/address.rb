cb311c75786b323b7f9a18f3d43c70a25701fcad
sotakari <146611649+sotakari@users.noreply.github.com>
2023-11-20 08:15:23 +0000

    マイページ完成 配送先 途中

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ApplicationRecord
+
+  belongs_to :customer
+
+  validates :postal_code, presence: true, length: { is: 7 }
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
