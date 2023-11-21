b95cfaa01f50e2ef76a7bee6f0172cf399c86c8a
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-11-21 09:43:43 +0000

    とりあえず2

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -51,7 +51,7 @@ class Public::CartItemsController < ApplicationController
   private
   
   def cart_item_params
-    params.require(:cart_items).permit(:item_id, :amount, :customer_id)
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
   end
 
 end
