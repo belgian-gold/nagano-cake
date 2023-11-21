d071ee2dc78d99e47c820a6e02a9a30c4771921d
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-11-21 05:14:40 +0000

    カート機能途中

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :cart_items, dependent: :destroy
+  # 会員:1はカートアイテム多
+
 end
