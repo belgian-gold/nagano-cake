c51593f482b418aaee5a6af59bca025aa4429a5c
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2023-11-22 05:47:09 +0000

    migration fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_20_093727) do
+ActiveRecord::Schema.define(version: 2023_11_22_054317) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -117,7 +117,7 @@ ActiveRecord::Schema.define(version: 2023_11_20_093727) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id", null: false
-    t.integer "payment_metho", default: 0, null: false
+    t.integer "payment_method", default: 0, null: false
     t.integer "total_price", null: false
     t.integer "postage", null: false
     t.string "addres", default: "", null: false
