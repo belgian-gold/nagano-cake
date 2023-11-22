752a3ae0f7707fd605196b63f30977af4a2fa63f
sotakari <146611649+sotakari@users.noreply.github.com>
2023-11-22 09:48:07 +0000

    退会機能 エラー

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :reject_customer, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -28,4 +29,15 @@ class Public::SessionsController < Devise::SessionsController
     root_path
   end
 
+  def reject_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_membership_status == false)
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
+        redirect_to new_customer_registration_path
+      else
+        flash[:notice] = "項目を入力してください"
+      end
+    end
+  end
 end
