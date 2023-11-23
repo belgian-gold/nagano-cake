0344e72b2ce95eb04db360e0abb67af704b1dbb6
sotakari <146611649+sotakari@users.noreply.github.com>
2023-11-23 05:51:49 +0000

    会員一覧

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,14 +1,24 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "変更が完了しました"
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
   end
 
 private
