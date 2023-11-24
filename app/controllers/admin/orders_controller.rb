ed82e5a1b57f1031e57d05a4ffaa32bcfbf38902
YoneiAtsushi <YoneiAtsushi@users.noreply.github.com>
2023-11-24 05:08:06 +0000

    admin注文履歴一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,27 @@
 class Admin::OrdersController < ApplicationController
+def index
+    @customer = Customer.find(params[:id])
+    @orders = @customer.orders.all
+  end
+
   def show
+    @order = Order.find(params[:id])
+    # @customer = Customer.find(params[:id])
+    @order_details = @order.order_details
+    @postage = 800
+    @subtotal = @order.total_payment - @postage
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
+    @order.update(order_params)
+
+    if @order.status == "confirm_payment"
+       @order_details.update(production_status:'waiting_for_production')
+    end
+
+      redirect_to admin_order_path(@order)
   end
 
   private
