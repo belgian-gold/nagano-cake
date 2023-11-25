d3273a24bdc166a28a61e1de8961e6dd0bc3f8b5
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2023-11-25 09:38:42 +0000

    order_details

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,40 +1,34 @@
 class Admin::OrdersController < ApplicationController
 def index
-  end
+end
 
   def show
      @order =Order.find(params[:id])
-     #@order_details= OrderDetail.where(order_id: @order.id)
+     @order_details= OrderDetail.where(order_id: @order.id)
      @order_details = @order.order_details
   end
 
   def update
-     @order = Order.find(params[:id])
+    @order = Order.find(params[:id])
     @order_details = @order.order_details
     @order.update(order_params)
-    if @order.status == "confirmation"
+    if @order.is_order_status == "confirmation"
       @order_details.each do |order_detail|
-        order_detail.making_status = 1
-        order_detail.update(order_detail_params)
+      order_detail.is_production_status = 1
+      order_detail.update(order_detail_params)
       end
     end
       redirect_to admin_order_path(@order)
   end
 
-  private
-  def order_params
-    params.require(:order).permit(:status)
-  end
-
-
 private
 
   def order_params
-    params.require(:order).permit(:status)
+    params.require(:order).permit(:is_order_status)
   end
 
   def order_detail_params
-    params.permit(:making_status)
+    params.permit(:is_production_status)
   end
 
 
