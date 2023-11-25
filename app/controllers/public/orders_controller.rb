735b1116c70506f11df01aa78220173db44e40b2
sotakari <146611649+sotakari@users.noreply.github.com>
2023-11-25 05:42:19 +0000

    注文履歴詳細画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -126,7 +126,7 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.where(order_id: params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
 
 end
