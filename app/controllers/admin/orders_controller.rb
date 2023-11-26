bfc271d8889171e06d67cc8129428108a95002a2
sotakari <146611649+sotakari@users.noreply.github.com>
2023-11-26 06:38:11 +0000

    諸々修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,7 @@
 class Admin::OrdersController < ApplicationController
 def index
+  @customer = Customer.find(params[:id])
+  @orders = Order.where(customer_id: @customer.id).order(created_at: :desc).page(params[:page]).per(10)
 end
 
   def show
@@ -9,23 +11,17 @@ end
   end
 
   def update
-     @order = Order.find(params[:id])
+    @order = Order.find(params[:id])
     @order_details = @order.order_details
     @order.update(order_params)
-    if @order.is_order_status == "confirmation"
-      @order_details.each do |order_detail|
+  if @order.is_order_status == "confirmation"
+        @order_details.each do |order_detail|
         order_detail.is_production_status = 1
         order_detail.update(order_detail_params)
-      end
     end
-      redirect_to admin_order_path(@order)
   end
-
-  private
-  def order_params
-    params.require(:order).permit(:status)
+    redirect_to admin_order_path(@order)
   end
-
 
 private
 
